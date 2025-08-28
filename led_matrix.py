9f30b77664652df931074f68d581c7bbb0e77400
altus111 <markusaltenburger@hotmail.com>
2025-08-28 12:43:55 +0200

    neueste

--- led_matrix.py
+++ led_matrix.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sense_hat import SenseHat
 from time import sleep
+import math
 sense = SenseHat()
 sense.clear()
-sleep(1)
 # examples using (x, y, r, g, b)
 '''
 sense.set_pixel(0, 0, 255, 0, 0)
@@ -47,15 +47,59 @@ sense.set_pixels(question_mark)
 '''
 max_x = 7
 my_y = 7
-origin = (0,7)
-sense.set_pixel(origin[0], origin[1], 255, 0, 0)
-for i in range(0,max_x+1):
-    sense.set_pixel(i, 0, 255, 0, 0)
-    print('i:',i)
-    sleep(1)
-print('fertig anzuenden')    
-for a in range(max_x,-1,-1):
-    sense.set_pixel(a, 0, 0, 0, 0)
-    print('a:',a)
-    sleep(1)
-print('fertig loeschen')   
+origin = (7,0)
+sense.set_pixel(origin[0], origin[1], 255, 255, 255)
+print('Nullpunkt',origin)
+def gerade():
+    for i in range(0,max_x+1):
+        sense.set_pixel(i, 0, 255, 0, 0)
+        print('i:',i)
+        sleep(1)
+    print('fertig anzuenden')    
+    for a in range(max_x,-1,-1):
+        sense.set_pixel(a, 0, 0, 0, 0)
+        print('a:',a)
+        sleep(1)
+    print('fertig loeschen')
+
+def kreis():
+    center_x = float(input("Zentrum in X :"))
+    print(center_x)
+    center_y = 3.5
+    radius = 3
+    color = (255,0,0)
+    for x in range(8):
+        for y in range(8):
+            distance = math.sqrt((x - center_x)**2 + (y - center_y)**2)
+            if abs(distance - radius) < 0.5:
+                sense.set_pixel(x, y, color)
+
+
+def draw_line(x0, y0, x1, y1, color):
+    #Bresenham-Algorithmus
+    try:
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+        sx = 1 if x0 < x1 else -1
+        sy = 1 if y0 < y1 else -1
+        err = dx - dy
+        doLoop = True
+        while doLoop: 
+            if 0 <= x0 < 8 and 0 <= y0 < 8:
+                sense.set_pixel(x0, y0, color)
+            if x0 == x1 and y0 == y1:
+                doLoop=False
+            e2 = 2 * err
+            if e2 > -dy:
+                err -= dy
+                x0 += sx
+            if e2 < dx:
+                err += dx
+                y0 += sy
+    except:
+        print(f'ERROR: Einer der Parameter {x0, y0, x1, y1} ist kein gueltiger Wert')
+  
+color = (0,255,0)  
+#draw_line(0,0,4,7,color)
+#kreis()   
+#linie()    

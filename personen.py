40755f1c4d33cb2768a152b9208e7d5d6c4f6205
altus111 <markusaltenburger@hotmail.com>
2025-09-09 18:11:08 +0200

    Automatischer Upload vom Raspberry Pi am Di 09 Sep 2025 18:11:08 CEST

--- personen.py
+++ personen.py
@@ -8,7 +8,7 @@ class Person:
         
     def __str__(self):
         return f'''
-            Vorname:{self.firstname}
+            Vorname:{self.__firstname}
             Nachname:{self.lastname}
         '''
     @property
@@ -18,20 +18,20 @@ class Person:
     @lastname.setter
     def lastname(self,new_lastName):
         self.__lastname = new_lastName
-
     @property
     def firstname(self): 
         return self.__firstname
-    
     @firstname.setter
-    def firstname(self,new_firstName):
-        self.__firstname = new_firstName
+    def lastname(self,new_firstName):
+        self.__lastname = __firstnameName
 
 if __name__ == '__main__':
     erst_person = Person(lastname='Altenburger',firstname='Markus')
     zweite_person = Person(lastname='Mueller',firstname='Peter')
     print(erst_person)
-    erst_person.firstname = "Peter"
-    print(erst_person.firstname)
-    zweite_person.lastname="Schlosser"
+    erst_person.firstnamename = "Peter"
+    print(e
+    rst_person.firstname)
+    zweite_person.lastname("Schlosser")
     print(zweite_person)
+    zweite_person = erst_person
